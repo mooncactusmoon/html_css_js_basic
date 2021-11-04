53bf053e722f2d773b1e5eb4eae482e2b3e50551
MoonLin <mooncactusmoon@gmail.com>
2021-11-04 15:13:29 +0800

    05for 新增一段code

--- y1104/05forloop.js
+++ y1104/05forloop.js
@@ -3,3 +3,6 @@ const myArr=['amy','bob','candy','dodo'];
 for(let index = 0 ; index < myArr.length ; index++){
     console.log(myArr[index]);
 }
+for(let i=0;i<5;i++){
+    console.log("i =" + i);
+}
