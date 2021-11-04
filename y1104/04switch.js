cffb0f97aca0068d1189f39d65dba9fc41c5cfdd
MoonLin <mooncactusmoon@gmail.com>
2021-11-04 14:34:04 +0800

    04 switch case 完成

---
+++ y1104/04switch.js
@@ -0,0 +1,19 @@
+let score = 59;
+switch (true) {
+    case score >= 90:
+        console.log(score + "AA :D");
+        break;
+    case score >= 80:
+        console.log(score + "BB :)");
+        break;
+    case score >= 70:
+        console.log(score + "CC :|");
+        break;
+    case score >= 60:
+        console.log(score + "DD :(");
+        break;
+    case score< 60:
+        console.log(score + "FF :<");
+        break;
+}
+
