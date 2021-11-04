808ae683ac57bca604e0e2e8aa211af67d898634
MoonLin <mooncactusmoon@gmail.com>
2021-11-04 15:47:00 +0800

    05for 新增一段For+陣列code

--- y1104/05forloop.js
+++ y1104/05forloop.js
@@ -1,8 +1,47 @@
-const myArr=['amy','bob','candy','dodo'];
+const iphoneObj={
+    "iphone8" : 8000,
+    "iphone9" : 10000,
+    "iphone11" : 15000,
+    "iphone12" : 20000,
+    "iphone13" : 32000,
+}
+console.log(iphoneObj);
+for(const[key, value] of Object.entries(iphoneObj)){
+    console.log(`${key}:${value}`)
+}
 
-for(let index = 0 ; index < myArr.length ; index++){
-    console.log(myArr[index]);
-}
-for(let i=0;i<5;i++){
-    console.log("i =" + i);
-}
+// const myArr=['amy','bob','candy','dodo'];
+// //js foreach
+// myArr.forEach(myFunc);
+// function myFunc(value,key){
+//     console.log(value);
+//     console.log(key);
+// }
+
+
+// const myArr=['amy','bob','candy','dodo'];
+// console.log(myArr.length);
+// console.log(myArr[3]);
+
+// for(let i in myArr){
+//     console.log("for in , i = " + i);
+// }
+// myArr.pop(); //刪除最後一個元素
+// myArr.push('egg'); //增加一個元素
+// console.log(myArr);
+// console.log(myArr.length);
+
+
+
+// const myArr=['amy','bob','candy','dodo'];
+// for(let i = 0 ; i < myArr.length ; i++){
+//     console.log(myArr[i]);
+// }
+//myArr.length 抓陣列長度
+//要先用let宣告變數
+
+
+
+// for(let i = 0 ; i < 5 ; i++){
+//     console.log("i =" + i);
+// }
